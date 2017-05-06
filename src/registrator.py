6d9b828f80f1c0431b8cd6148e3bd87abfa1c467
areong <areong6378@gmail.com>
2017-05-06 22:08:45 +0800

    Improve UI, catch decoding exception, and add gitignore.
    Print the total number of mail addresses and the index number of each mail address to let users know the progress. Pause and wait for user input at the end of the program. If there are invalid characters in the mail addresses that cause decoding error, terminate the program.

--- src/registrator.py
+++ src/registrator.py
@@ -5,16 +5,23 @@ import urllib.request
 def read_mail_addresses(filename):
     '''
     Read the mail address file and return the mail address list.
-    If there is any non-ASCII character, reading may fail.
+    Do not check the address format. Just read all lines.
     '''
     mail_addresses = []
+    read_successfully = True
     with open(filename, 'r') as file:
-        mail_addresses = [line.strip() for line in file]
-    return mail_addresses
+        try:
+            mail_addresses = [line.strip() for line in file]
+        except UnicodeDecodeError:
+            print('There are invalid characters in ' + filename + '.')
+            print('It is recommended to use ASCII charactors only.')
+            read_successfully = False
+    return mail_addresses, read_successfully
 
 def registrate(mail):
     '''
-    Registrate the mail and return whether it succeeded or not.
+    Registrate the mail to the official site.
+    Return whether it registrated successfully.
     '''
     url = 'https://prereg.bn-ent.net/common/serial.php'
     values = {
@@ -42,37 +49,43 @@ def registrate(mail):
 def main(mail_addresses_filename):
     '''
     Input:
-    mail_addresses_filename     the filename of the file containing mail addresses.
+    mail_addresses_filename     the filename of the file containing the mail addresses.
     '''
     print('===================================================================')
     print('  THE IDOLM@STER MILLION LIVE! THEATER DAYS Preregistration Tool   ')
     print('===================================================================')
 
     # Read mail addresses.
-    mail_addresses = read_mail_addresses(mail_addresses_filename)
-    print('Read ' + mail_addresses_filename + '.')
+    mail_addresses, read_successfully = read_mail_addresses(mail_addresses_filename)
+    if not read_successfully:
+        return 0
+    print('There are ' + str(len(mail_addresses)) + ' mail addresses in ' + mail_addresses_filename + '.')
 
     # Registrate one by one.
     print('Start registrating each mail address:')
-    successCount = 0
+    mail_addresses_count = 1
+    success_count = 0
     for mail_address in mail_addresses:
-        print('  ' + mail_address + '  ' + '.' * (50 - len(mail_address)), end='')
+        print(str(mail_addresses_count) + '. ' + mail_address + '  ' + '.' * (55 - len(str(mail_addresses_count)) - len(mail_address)), end='')
         if registrate(mail_address):
             print('succeeded')
-            successCount += 1
+            success_count += 1
         else:
             print('failed')
+        mail_addresses_count += 1
 
     # Show the overall result.
-    print('\nSuccessfully registrate ' + str(successCount) + ' out of ' + str(len(mail_addresses)) + ' mail addresses! Keep going!')
+    print('\nSuccessfully registrate ' + str(success_count) + ' out of ' + str(len(mail_addresses)) + ' mail addresses! Keep going!')
 
 if __name__ == '__main__':
     # Parse the inputs.
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--input', default='mail_addresses.txt',
-        help='set the file containing the mail addresses. The default filename is \'mail_addresses.txt\'.')
+        help='set the file which contains the mail addresses. The default filename is \'mail_addresses.txt\'.')
     args = parser.parse_args()
     if args.input:
         mail_addresses_filename = args.input
 
     main(mail_addresses_filename)
+
+    input('Press enter to terminate...')
